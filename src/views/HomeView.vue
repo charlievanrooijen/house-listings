095a3a6932d386692abb6c34cdabc24d94b25e61
charlievanrooijen <78792797+charlievanrooijen@users.noreply.github.com>
2024-05-15 13:45:02 +0200

    Multiple pages and navigation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Add any global styles here */
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+nav li {
+  display: inline;
+  margin-right: 10px;
+}
+
+nav a {
+  text-decoration: none;
+  color: inherit;
+}
+
+nav a.router-link-active {
+  font-weight: bold;
+}
+</style>
