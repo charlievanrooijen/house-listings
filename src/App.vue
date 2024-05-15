44ca0953129fd93a1c0ef98583001017e237994f
charlievanrooijen <78792797+charlievanrooijen@users.noreply.github.com>
2024-05-15 13:28:02 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* Voeg hier je stijlen toe als dat nodig is */
+</style>
