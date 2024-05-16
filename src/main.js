139f7bdccec29d9b13064c8f2b0af425094fe6ae
charlievanrooijen <78792797+charlievanrooijen@users.noreply.github.com>
2024-05-16 22:46:03 +0200

    added fontawesome to packages

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
+// src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
-import './custom.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import './global.scss';
 
 import BootstrapVue3 from 'bootstrap-vue-3';
+
+// Import FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Add icons to the library
+library.add(faUser, faSignOutAlt);
 
 const app = createApp(App);
 
 app.use(router);
 app.use(BootstrapVue3);
+
+// Register the FontAwesomeIcon component globally
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
