319bd9e6fff2a27485fd1b9074e90f87748c448f
charlievanrooijen <78792797+charlievanrooijen@users.noreply.github.com>
2024-05-17 20:44:27 +0200

    fixed the edit functionality

--- src/assets/scripts/EditListing.js
+++ src/assets/scripts/EditListing.js
@@ -29,24 +29,8 @@ export default {
   },
   created() {
     this.fetchHouseDetails();
-    if (this.house) {
-      this.form = { ...this.house };
-    }
   },
   methods: {
-    async submitForm() {
-      try {
-        const response = await axios.put(`https://api.intern.d-tt.nl/api/houses/${this.house.id}`, this.form, {
-          headers: {
-            'X-Api-Key': process.env.VUE_APP_API_KEY
-          }
-        });
-        console.log('Listing updated successfully:', response.data);
-        this.$router.push('/');
-      } catch (error) {
-        console.error('Error updating listing:', error.response ? error.response.data : error.message);
-      }
-    },
     async fetchHouseDetails() {
       const houseId = this.$route.params.id;
       try {
@@ -56,9 +40,52 @@ export default {
           }
         });
         this.house = response.data[0];
-        console.log(response.data)
+        // Initialize the form with the house details
+        if (this.house) {
+          this.form = {
+            price: this.house.price,
+            bedrooms: this.house.rooms.bedrooms,
+            bathrooms: this.house.rooms.bathrooms,
+            size: this.house.size,
+            streetName: this.house.location.street,
+            houseNumber: this.house.location.houseNumber,
+            numberAddition: this.house.location.houseNumberAddition,
+            zip: this.house.location.zip,
+            city: this.house.location.city,
+            constructionYear: this.house.constructionYear,
+            hasGarage: this.house.hasGarage,
+            description: this.house.description
+          };
+        }
       } catch (error) {
         console.error('Error fetching house details:', error.response ? error.response.data : error.message);
+      }
+    },
+    async submitForm() {
+      try {
+        const response = await axios.post(`https://api.intern.d-tt.nl/api/houses/${this.house.id}`, {
+          price: this.form.price,
+          bedrooms: this.form.bedrooms,
+          bathrooms: this.form.bathrooms,
+          size: this.form.size,
+          streetName: this.form.streetName,
+          houseNumber: this.form.houseNumber,
+          houseNumberAddition: this.form.numberAddition,
+          zip: this.form.zip,
+          city: this.form.city,
+          constructionYear: this.form.constructionYear,
+          hasGarage: this.form.hasGarage,
+          description: this.form.description
+        }, 
+        {
+          headers: {
+            'X-Api-Key': process.env.VUE_APP_API_KEY
+          }
+        });
+        console.log('Listing updated successfully:', response.data);
+        this.$router.push('/edit/' + this.house.id);
+      } catch (error) {
+        console.error('Error updating listing:', error.response ? error.response.data : error.message);
       }
     },
     async deleteListing() {
