169f13b92219fe79d66905fdcd94573ba8c934ce
charlievanrooijen <78792797+charlievanrooijen@users.noreply.github.com>
2024-05-26 15:46:52 +0200

    styling changes to the detailpage

--- src/views/CreateListingView.vue
+++ src/views/CreateListingView.vue
@@ -42,8 +42,8 @@
         <div class="inputContainer mb-3">
           <label for="uploadPicture" class="form-label">Upload picture (PNG or JPG)*</label>
           <input style="display: none;" @change="handleFileChange" accept="image/*" type='file' id="imginput" />
-          <div id="imagePreviewContainer" style="display: none; position: relative;" class="w-50">
-            <img class="w-100" id="imgPreview" src="" alt="your image" />
+          <div id="imagePreviewContainer" style="display: none" class="image-preview-container w-50">
+            <img class="rounded house-image-list" id="imgPreview" src="" alt="your image" />
             <img id="clearImage" src="../assets/images/ic_clear_white@3x.png" @click="unloadImagePreview()" />
           </div>
           <div @click="triggerFileInput">
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container-lg  bg-transparent pagecontainer">
-    <div class="my-3 top-nav-linkwrapper w-100">
+  <div class="container-lg bg-transparent">
+    <div class="top-nav-linkwrapper w-100">
       <router-link style="text-decoration: none" to="/">
-        <h3 class="text-dark topnavlink-mobile"><i class="mobile-arrow fa-solid fa-arrow-left"> </i> <span
-            class="desktop-return"> Back to overview </span> </h3>
+        <h3 class="text-dark py-lg-3">
+          <i class="mobile-arrow fa-solid fa-arrow-left"></i>
+          <span class="desktop-return">Back to overview</span>
+        </h3>
       </router-link>
-      <div class="absolute-top-right mobile-delete" v-if="house && house.madeByMe">
-        <div class="row p-3  m-0">
+      <div class="mobile-delete" v-if="house && house.madeByMe">
+        <div class="row">
           <div class="col-6 p-0 m-0 d-flex justify-content-center">
             <router-link :to="{ name: 'edit', params: { id: house.id } }">
               <img class="w-50" src="../assets/images/ic_edit_white@3x.png">
@@ -24,16 +26,12 @@
       <div class="row">
         <div class="col-12 p-0 col-lg-7">
           <div class="image-wrapper" v-if="house.image">
-            <img :src="house.image" :alt="house.name" class="house-image m-0 w-100" />
+            <img :src="house.image" :alt="house.name" class="house-image m-0 w-100">
           </div>
           <div class="house-detail-container bg-light w-100">
             <div class="row">
               <div class="col-12 p-0">
-                <h1 class="p-2">
-                  {{ house.location.street }} {{ house.location.houseNumber }} {{ house.location.houseNumberAddition }}
-                </h1>
-              </div>
-              <div class="col-6">
+                <h1 class="p-2">{{ house.location.street }} {{ house.location.houseNumber }} {{ house.location.houseNumberAddition }}</h1>
               </div>
             </div>
             <div class="location house-detail icon-wrapper d-flex">
@@ -41,55 +39,71 @@
                 <img class="image-icon" src="../assets/images/ic_location@3x.png">
                 {{ house.location.zip }}, {{ house.location.city }}
               </h3>
-              <div class="absolute-top-right desktop-delete p-4" v-if="house.madeByMe">
+              <div class="edit-delete-container desktop-delete p-3 p-lg-0 mt-3" v-if="house.madeByMe">
                 <router-link :to="{ name: 'edit', params: { id: house.id } }">
-                  <img class="w-25" style="margin-right: 2rem;" src="../assets/images/ic_edit@3x.png">
+                  <img class="w-50 p-3 p-lg-4" src="../assets/images/ic_edit@3x.png">
                 </router-link>
                 <a @click.stop="showDeleteModal(house.id)">
-                  <img class="w-25" src="../assets/images/ic_delete@3x.png">
+                  <img class="w-50 p-3 p-lg-4" src="../assets/images/ic_delete@3x.png">
                 </a>
               </div>
             </div>
             <div class="house-detail icon-wrapper icon-container">
-              <span class="pr-3 pt-3 pb-3 text-dark text-left"><img class="image-icon"
-                  src="../assets/images/ic_price@3x.png"> {{ $currencyWithoutSign(house.price) }}</span>
-              <span class="pr-3 pt-3 pb-3 text-dark text-left"><img class="image-icon"
-                  src="../assets/images/ic_size@3x.png"> {{ house.size }} m²</span>
-              <span class="pr-3 pt-3 pb-3 text-dark text-left"><img class="image-icon"
-                  src="../assets/images/ic_construction_date@3x.png"> Built in {{ house.constructionYear }}</span>
+              <span class="pr-3 pt-3 pb-3 text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_price@3x.png">
+                {{ $currencyWithoutSign(house.price) }}
+              </span>
+              <span class="pr-3 pt-3 pb-3 text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_size@3x.png">
+                {{ house.size }} m²
+              </span>
+              <span class="pr-3 pt-3 pb-3 text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_construction_date@3x.png">
+                Built in {{ house.constructionYear }}
+              </span>
             </div>
             <div class="house-detail listing-information icon-wrapper icon-container">
-              <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_bed@3x.png"> {{
-                house.rooms.bedrooms }}</span>
-              <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_bath@3x.png">
-                {{
-                  house.rooms.bathrooms }} </span>
-              <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_garage@3x.png">
-                {{
-                  house.hasGarage ? "yes" : "no" }}</span>
+              <span class="text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_bed@3x.png">
+                {{ house.rooms.bedrooms }}
+              </span>
+              <span class="text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_bath@3x.png">
+                {{ house.rooms.bathrooms }}
+              </span>
+              <span class="text-dark text-left">
+                <img class="image-icon" src="../assets/images/ic_garage@3x.png">
+                {{ house.hasGarage ? "yes" : "no" }}
+              </span>
             </div>
-            <div>
-              <p class=" py-3">{{ house.description }}</p>
+            <div class="house-detail p-0">
+              <p class="py-3">{{ house.description }}</p>
             </div>
           </div>
         </div>
         <div class="col-12 col-lg-5 px-lg-5 px-4">
-          <h1 class="m-3">Recommended for you</h1>
+          <h1 class="mx-3 ,mb-3">Recommended for you</h1>
           <div class="row bg-light my-1 p-2 rounded mx-1">
             <div class="col-5 d-flex align-items-center">
-              <img :src="house.image" :alt="house.name" class="house-image rounded m-0" />
+              <img :src="house.image" :alt="house.name" class="house-image rounded m-0">
             </div>
             <div class="col-7 p-0 p-lg-2 p-1">
               <h2 class="p-0 m-0 street">Stokvisstraat 321</h2>
               <h2 class="pt-2 m-0 price">€500.000</h2>
               <h3 class="pt-2 m-0 adress">1011 AA Amsterdam</h3>
               <div class="house-detail icon-wrapper icon-container pt-3">
-                <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_bed@3x.png"> {{
-                  house.rooms.bedrooms }}</span>
-                <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_bath@3x.png"> {{
-                  house.rooms.bathrooms }} </span>
-                <span class="text-dark text-left"><img class="image-icon" src="../assets/images/ic_garage@3x.png"> {{
-                  house.hasGarage ? "yes" : "no" }}</span>
+                <span class="text-dark text-left">
+                  <img class="image-icon" src="../assets/images/ic_bed@3x.png">
+                  {{ house.rooms.bedrooms }}
+                </span>
+                <span class="text-dark text-left">
+                  <img class="image-icon" src="../assets/images/ic_bath@3x.png">
+                  {{ house.rooms.bathrooms }}
+                </span>
+                <span class="text-dark text-left">
+                  <img class="image-icon" src="../assets/images/ic_garage@3x.png">
+                  {{ house.hasGarage ? "yes" : "no" }}
+                </span>
               </div>
             </div>
           </div>
@@ -100,21 +114,21 @@
       <p>Loading house details...</p>
     </div>
   </div>
-      <div v-if="showModal" class="modal-overlay">
-        <div class="modal-content bg-light">
-          <h2 class="bold">Delete listing</h2>
-          <h3 class="mt-4 text-center m-0">Are you sure you want to delete this listing?</h3>
-          <h3 class="mb-4 text-center">This action cannot be undone</h3>
-          <div class="modal-actions row">
-            <div class="col-12 mb-3">
-              <button @click="confirmDelete" class="bold btn btn-primary text-light w-75">YES DELETE</button>
-            </div>
-            <div class="col-12">
-              <button @click="closeModal" class="bold btn btn-secondary text-light w-75">GO BACK</button>
-            </div>
-          </div>
+  <div v-if="showModal" class="modal-overlay">
+    <div class="modal-content bg-light">
+      <h2 class="bold">Delete listing</h2>
+      <h3 class="mt-4 text-center m-0">Are you sure you want to delete this listing?</h3>
+      <h3 class="mb-4 text-center">This action cannot be undone</h3>
+      <div class="modal-actions row">
+        <div class="col-12 mb-3">
+          <button @click="confirmDelete" class="bold btn btn-primary text-light w-75">YES DELETE</button>
+        </div>
+        <div class="col-12">
+          <button @click="closeModal" class="bold btn btn-secondary text-light w-75">GO BACK</button>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script src="../assets/scripts/DetailView.js"></script>
